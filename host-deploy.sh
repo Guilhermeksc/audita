1492aee5396b2250ee16f41be1d73286da7a3a55
GuilhermeK <guilhermekscampos@gmail.com>
2025-03-30 22:28:46 -0300

    att

--- host-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "[HOST-DEPLOY] Iniciando deploy..." >> /var/log/deploy.log
-
-# Executa o script dentro do container host (como root)
-bash /root/audita/scripts/deploy.sh >> /var/log/deploy.log 2>&1
