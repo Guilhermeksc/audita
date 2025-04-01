f9610b9c7776c154cf6c7f4fef209d8dd861fd69
GuilhermeK <guilhermekscampos@gmail.com>
2025-03-31 21:04:39 -0300

    att

--- backend/django/backend/users/admin.py
+++ backend/django/backend/users/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import Usuario, Perfil
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = Usuario
+        fields = ('nip', 'nome_completo', 'posto')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
+        
 @admin.register(Perfil)
 class PerfilAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome')
@@ -10,43 +21,35 @@ class PerfilAdmin(admin.ModelAdmin):
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
     model = Usuario
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
     ordering = ['nip']
     list_display = (
-        'nip',
-        'nome_completo',
-        'nome_de_guerra',
-        'posto',
-        'especialidade',
-        'nome_funcao',
-        'divisao',
-        'email',
-        'get_perfis',
-        'is_staff',
+        'nip', 'nome_completo', 'nome_de_guerra', 'posto',
+        'especialidade', 'nome_funcao', 'divisao', 'email',
+        'get_perfis', 'is_staff',
     )
     search_fields = ('nip', 'nome_completo', 'nome_de_guerra', 'email')
-    filter_horizontal = ('perfis',)  # interface amigável para ManyToMany
+    filter_horizontal = ('perfis',)
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': (
-                'nip',
-                'password1',
-                'password2',
-                'nome_completo',
-                'posto',
-            ),
+            'fields': ('nip', 'password1', 'password2', 'nome_completo', 'posto'),
         }),
     )
+
     def add_view(self, request, form_url='', extra_context=None):
         if request.method == 'POST':
-            form = self.get_form(request)(request.POST)
+            form_class = self.get_form(request, obj=None, change=False)
+            form = form_class(request.POST)
             if not form.is_valid():
                 print("\n== ERROS NO FORMULÁRIO ==")
                 for field, errors in form.errors.items():
                     print(f"{field}: {errors}")
                 print("== FIM DOS ERROS ==\n")
         return super().add_view(request, form_url, extra_context)
-
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
@@ -63,30 +66,19 @@ class UsuarioAdmin(UserAdmin):
             (None, {'fields': ('nip', 'password')}),
             ('Informações pessoais', {
                 'fields': (
-                    'nome_completo',
-                    'nome_de_guerra',
-                    'posto',
-                    'especialidade',
-                    'nome_funcao',
-                    'divisao',
-                    'email',
+                    'nome_completo', 'nome_de_guerra', 'posto', 'especialidade',
+                    'nome_funcao', 'divisao', 'email',
                 )
             }),
             ('Permissões', {
                 'fields': (
-                    'is_active',
-                    'is_staff',
-                    'is_superuser',
-                    'groups',
-                    'user_permissions',
+                    'is_active', 'is_staff', 'is_superuser',
+                    'groups', 'user_permissions',
                 )
             }),
         )
-
-        # perfil_ativo será apenas editável após criação
-        if obj:  # Ou seja, já foi salvo
+        if obj:
             base_fieldsets[1][1]['fields'] += ('perfil_ativo',)
-
         if request.user.is_superuser:
             return base_fieldsets + (('Perfis do Sistema', {'fields': ('perfis',)}),)
         return base_fieldsets
