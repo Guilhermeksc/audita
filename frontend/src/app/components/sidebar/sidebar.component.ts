594099524c0bf72e0c6898f3b18d26271fb52ee9
Guilhermeksc <guilhermekscampos@gmail.com>
2025-04-02 13:52:54 -0300

    att

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -31,6 +31,21 @@ export class SidebarComponent {
   menuItems: MenuItem[] = [
     { title: 'PAINT-RAINT', icon: 'assets/icons/svg/menu_icon.svg', children: [
       {
+        title: 'Subsídios',
+        children: [
+          'CCIMAR-12 - Licitação',
+          'CCIMAR-13 - Execução Financeira',
+          'CCIMAR-14 - Pagamento',
+          'CCIMAR-15 - Material', 
+          'Última Auditoria',
+          'Nº de Notas de Auditoria',
+          'Foco Externo',
+          'OC',
+          'OMPS'
+        ],
+        expanded: false
+      },      
+      {
         title: 'PAINT',
         children: [
           'Cadastro Objetos Auditáveis',
@@ -40,89 +55,96 @@ export class SidebarComponent {
           'Anexo B - Organizações Militares Representativas'
         ],
         expanded: false
-      },
-      'RAINT'
-    ], expanded: true },
-    { title: 'PLANEJAMENTO', icon: 'assets/icons/svg/menu_icon.svg', children: [
-      'Geração de OS',
-      'Minuta de MSG',
-      'Ofício de Equipe',
-      'Matriz de Riscos',
-      'Plano de Auditoria',
-      'S.A',
-      'Apresentação Inicial',
-    ],
-    expanded: true
-    }, 
-    { title: 'EXECUÇÃO', icon: 'assets/icons/svg/menu_icon.svg', children: [
-      'Testes com base na Matriz de Planejamento',
-      'PT',
-      'Matriz de Achados',
+      }, 
       {
-        title: 'Relatórios',
-        children: ['Relatório Preliminar', 'Relatório Final'],
+        title: 'Ciclos',
+        children: [
+          'Cadastro Organizações Auditáveis',
+        ],
         expanded: false
-      }
-    ],
-    expanded: true
-    },   
-    { title: 'MONITORAMENTO', icon: 'assets/icons/svg/menu_icon.svg', children: [
-      'Monitoramento'
-    ],
-    expanded: true
-    },
-    { title: 'Atos de Pessoal', icon: 'assets/icons/svg/menu_icon.svg', children: [
-      'Folha de Pagamento',
-      'Integração/Importação da base de dados do e-pessoal e conecta-TCU',
-      'Indícios do e-pessoal',
-      'Envio para TCU e recebimento dos Acórdãos',
-    ],
-    expanded: true
-    },      
-    { title: 'PGMQ', icon: 'assets/icons/svg/menu_icon.svg', children: [
-      'Acompanhamento das etapas do processo de Auditoria',
-      'Alerta de supervisionamento à conclusão de cada etapa',
-      'Avaliação periódica (modelo IA-CM)',
-    ],
-    expanded: true
-    }, 
-    { title: 'ANÁLISE DE RISCOS', icon: 'assets/icons/svg/menu_icon.svg', children: [
-      'Inserção de dados do TCU/TCE para subsidiar o PAINT (Matriz de riscos)',
-      'Definição de escopo',
-      'Importar planilhas de acompanhamento retroativas',
-      'Extração de relatórios para Solicitação de Auditoria e para COFAMAR',
-    ],
-    expanded: true
-    },     
-    { title: 'Assessoria de Controle Interno', icon: 'assets/icons/svg/menu_icon.svg', children: [
-      'Acompanhamento das demandas de ACI',
-      'Monitoramento das Assessorias sobre as OM subordinadas',
-      'Tramitar informações entre as ACI e as OM subordinadas',
-    ],
-    expanded: true
-    },               
-    { title: 'CCIMAR-12', icon: 'assets/icons/svg/menu_icon.svg', children: [
-        {
-          title: 'Diário Oficial da União (DOU)',
-          children: ['Acompanhamento', 'Pesquisar Histórico', 'Pesquisar data específica'],
-          expanded: false
-        },
-        'Análise Processual de Alta Materialidade',
-        {
-          title: 'Preço homologado acima do Estimado',
-          children: ['Dispensa Eletrônica', 'Pregão Eletrônico', 'Credenciamento', 'Concorrência'],
-          expanded: false
-        },
-        'Fragilidade da Estimativa da Demanda',
-        'Restrição para Contratação'
+      },      
+      'RAINT'
+      ], expanded: true },
+      { title: 'PLANEJAMENTO', icon: 'assets/icons/svg/menu_icon.svg', children: [
+        'Geração de OS',
+        'Minuta de MSG',
+        'Ofício de Equipe',
+        'Matriz de Riscos',
+        'Plano de Auditoria',
+        'S.A',
+        'Apresentação Inicial',
       ],
       expanded: true
-    },
-    { title: 'CCIMAR-13', icon: 'assets/icons/svg/menu_icon.svg', children: ['Despesa', 'Orçamento'], expanded: true },
-    { title: 'CCIMAR-14', icon: 'assets/icons/svg/menu_icon.svg', children: ['Pessoal'], expanded: true },
-    { title: 'CCIMAR-15', icon: 'assets/icons/svg/menu_icon.svg', children: ['Patrimônio', 'Subsistência'], expanded: true },
-    { title: 'CCIMAR-16', icon: 'assets/icons/svg/menu_icon.svg', children: ['Webscraping', 'API', 'RPA'], expanded: true },
-  ];
+      }, 
+      { title: 'EXECUÇÃO', icon: 'assets/icons/svg/menu_icon.svg', children: [
+        'Testes com base na Matriz de Planejamento',
+        'PT',
+        'Matriz de Achados',
+        {
+          title: 'Relatórios',
+          children: ['Relatório Preliminar', 'Relatório Final'],
+          expanded: false
+        }
+      ],
+      expanded: true
+      },   
+      { title: 'MONITORAMENTO', icon: 'assets/icons/svg/menu_icon.svg', children: [
+        'Monitoramento'
+      ],
+      expanded: true
+      },
+      { title: 'Atos de Pessoal', icon: 'assets/icons/svg/menu_icon.svg', children: [
+        'Folha de Pagamento',
+        'Integração/Importação da base de dados do e-pessoal e conecta-TCU',
+        'Indícios do e-pessoal',
+        'Envio para TCU e recebimento dos Acórdãos',
+      ],
+      expanded: true
+      },      
+      { title: 'PGMQ', icon: 'assets/icons/svg/menu_icon.svg', children: [
+        'Acompanhamento das etapas do processo de Auditoria',
+        'Alerta de supervisionamento à conclusão de cada etapa',
+        'Avaliação periódica (modelo IA-CM)',
+      ],
+      expanded: true
+      }, 
+      { title: 'ANÁLISE DE RISCOS', icon: 'assets/icons/svg/menu_icon.svg', children: [
+        'Inserção de dados do TCU/TCE para subsidiar o PAINT (Matriz de riscos)',
+        'Definição de escopo',
+        'Importar planilhas de acompanhamento retroativas',
+        'Extração de relatórios para Solicitação de Auditoria e para COFAMAR',
+      ],
+      expanded: true
+      },     
+      { title: 'Assessoria de Controle Interno', icon: 'assets/icons/svg/menu_icon.svg', children: [
+        'Acompanhamento das demandas de ACI',
+        'Monitoramento das Assessorias sobre as OM subordinadas',
+        'Tramitar informações entre as ACI e as OM subordinadas',
+      ],
+      expanded: true
+      },               
+      { title: 'CCIMAR-12', icon: 'assets/icons/svg/menu_icon.svg', children: [
+          {
+            title: 'Diário Oficial da União (DOU)',
+            children: ['Acompanhamento', 'Pesquisar Histórico', 'Pesquisar data específica'],
+            expanded: false
+          },
+          'Análise Processual de Alta Materialidade',
+          {
+            title: 'Preço homologado acima do Estimado',
+            children: ['Dispensa Eletrônica', 'Pregão Eletrônico', 'Credenciamento', 'Concorrência'],
+            expanded: false
+          },
+          'Fragilidade da Estimativa da Demanda',
+          'Restrição para Contratação'
+        ],
+        expanded: true
+      },
+      { title: 'CCIMAR-13', icon: 'assets/icons/svg/menu_icon.svg', children: ['Despesa', 'Orçamento'], expanded: true },
+      { title: 'CCIMAR-14', icon: 'assets/icons/svg/menu_icon.svg', children: ['Pessoal'], expanded: true },
+      { title: 'CCIMAR-15', icon: 'assets/icons/svg/menu_icon.svg', children: ['Patrimônio', 'Subsistência'], expanded: true },
+      { title: 'CCIMAR-16', icon: 'assets/icons/svg/menu_icon.svg', children: ['Webscraping', 'API', 'RPA'], expanded: true },
+    ];
 
   private routeMap: { [key: string]: string } = {
     'PAINT': 'paint',
